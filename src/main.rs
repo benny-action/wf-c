30eaf301b1107c9d92c9420a81824c15706dc953
benny <contact.ben.perkins@gmail.com>
2025-07-03 23:40:19 +0100

    tile impl, system to manage grid needed

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,47 @@
 use piston_window::*;
 
+const WIDTH: i32 = 512;
+const HEIGHT: i32 = 512;
+
+#[derive(Clone, Debug)]
+pub struct Tile {
+    pub colour: [f32; 4],
+    pub tile_type: TileType,
+    pub visible: bool,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum TileType {
+    Empty,
+    Wall,
+    Floor,
+    Water,
+}
+impl Tile {
+    pub fn new(tile_type: TileType, colour: [f32; 4]) -> Self {
+        Tile {
+            colour,
+            tile_type,
+            visible: true,
+        }
+    }
+
+    pub fn empty() -> Self {
+        Tile::new(TileType::Empty, [0.0, 0.0, 0.0, 0.0])
+    }
+    pub fn wall() -> Self {
+        Tile::new(TileType::Wall, [0.5, 0.5, 0.5, 1.0])
+    }
+    pub fn floor() -> Self {
+        Tile::new(TileType::Floor, [0.8, 0.7, 0.6, 1.0])
+    }
+    pub fn water() -> Self {
+        Tile::new(TileType::Water, [0.2, 0.4, 0.8, 1.0])
+    }
+}
+
+struct TileSystem {}
+
 fn main() {
     let mut window: PistonWindow = WindowSettings::new("WaveFunctionCollapse", [512; 2])
         .exit_on_esc(true)
